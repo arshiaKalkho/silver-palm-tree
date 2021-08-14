eb337aceb3e29650c5b907fab2cd0d6d357197e5
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-08-14 02:36:02 -0700

    git add --a

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Main() {
+  return <h1> this is the main component</h1>;
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
