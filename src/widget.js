19411e8af540e93a2419e701853993e480f267f8
arshia <arshiabdol@yahoo.com>
2021-09-20 03:30:02 -0400

    added json files and experiences and skills

---
+++ src/widget.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Skill from './skill'
+
+export class widget extends Component {
+    
+    
+    
+    render() {
+        return (
+            <div className="widget">
+                <Skill/>
+            </div>
+        )
+    }
+}
+
+export default widget;
