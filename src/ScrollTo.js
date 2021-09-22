dad2c515d0e8aeb35561eda6379d5409d9752266
arshia <arshiabdol@yahoo.com>
2021-09-22 19:31:19 -0400

    changed the background and addingmore now

---
+++ src/ScrollTo.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function ScrollTo(props) {
+ const location = props.location;
+ const text = props.text;
+  // Show button when page is scorlled upto given distance
+  
+
+  // Set the top cordinate to 0
+  // make scrolling smooth
+  const scrollTo = () => {
+    window.scrollTo({
+      top: location,
+      behavior: "smooth"
+    });
+  };
+
+ 
+
+  return (
+    <div className="scroll-to-top">
+      
+        <button className="btn" onClick={scrollTo}>
+         {text}
+        </button>
+    </div>
+  );
+}
---
+++ src/Summery.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Summery() {
+    return (
+        <div className="summery-text">
+            <p>I always loved computers, from a young age, as soon as i modified my first html page i was hooked</p>
+        </div>
+    )
+}
+
+export default Summery
