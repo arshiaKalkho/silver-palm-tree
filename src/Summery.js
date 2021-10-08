116205381dc649725257691ef9eee8a5095d23b0
arshia <arshiabdol@yahoo.com>
2021-10-08 15:30:54 -0400

    syncing

--- src/Summery.js
+++ src/Summery.js
@@ -7,15 +7,9 @@ function Summery() {
 
         
             <div className="summery-text">
-          <b>I </b>always loved computers, from a young age, as soon as I modified my first HTML page I was hooked, I wanted to understand how computers worked  on every possible level.I started from hardware CPUs, GPUs, memory... I learned as much as I could until I remembered how fun modifying
-                 that HTML page was, So, I learned programming.
-          
-            
-           
-                One year later I took my first computer science course in high school and realized what I wanted to do, I went to college and studied software.
-          
-            
-                 After graduating I thought my computer learning journey was over, But now I realize it's just the beginning.
+          <b>I </b>always loved computers, from a young age, as soon as I modified my first HTML page I was hooked,  I wanted to understand how computers worked  on every possible level.I started from hardware CPUs, GPUs,  memory... I learned as much as I could until I remembered how fun modifying that HTML page was, So I learned programming.
+          <br/>
+           One year later I took my first computer science course in high school and realized what I wanted to do, I went to college and studied software. After graduating I thought my computer learning journey was over, But now I realize it's just the beginning.
             
             </div>
         
