5c03cda4d0ca944be212965c2b18fb54f7b123bb
arshia <arshiabdol@yahoo.com>
2021-10-08 16:23:35 -0400

    summery alignment

--- src/Summery.js
+++ src/Summery.js
@@ -6,11 +6,26 @@ function Summery() {
         
 
         
-            <div className="summery-text">
-          <b>I </b>always loved computers, from a young age, as soon as I modified my first HTML page I was hooked,  I wanted to understand how computers worked  on every possible level.I started from hardware CPUs, GPUs,  memory... I learned as much as I could until I remembered how fun modifying that HTML page was, So I learned programming.
-          <br/>
-           One year later I took my first computer science course in high school and realized what I wanted to do, I went to college and studied software. After graduating I thought my computer learning journey was over, But now I realize it's just the beginning.
-            
+            <div className="summery-text"  >
+                <h3>Why computers</h3>
+                <p data-aos="flip-up"><b>I </b>always loved computers, from a young age, as soon as I modified my first HTML page I was hooked, I wanted to understand how computers worked  on every possible level. The complexity in the architecture of a computer made me want to learn more about it's inner workings, there was just something about it that I could never let go of. 
+                </p>
+                
+                <h3>Why programming</h3>
+                <p data-aos="flip-up"><b>I'm </b> a beauty behind the madness kind of a person, As a child, I wanted to learn everything, open every toy to learn it's make-up. Still, I love learning about complex topics like astrophysics, quantum computing, etc. I also enjoy a good challenge these two factors together plus my eternal love for computers made this an easy choice.
+                </p>
+                
+
+                <h3>My Education</h3>
+                <p data-aos="flip-up">
+                I studied Computer programming at <strong>Seneca</strong> and received an <strong>Advanced Diploma</strong> in <strong>Computer programming and analysis</strong>. There I learned to work in an agile or scrum environment, trained in multiple languages including C++, Java, Javascript. I was thought the sentence "Git Good at Git", The logic behind the leyers of internet, the basics of networking, the importance of writing efficient code using <strong>Data Strutures/Dynamic Programming</strong>, using SQL and NoSQL databases...
+                </p>
+                <h3>My Personal life</h3>
+                <p data-aos="flip-up">
+                I learn on a daily basis, whether it is a new language, algorithms or skills, I take challenges and am not afraid to fail. check my GitHub for the projects that I have created in order to learn and challenge myself including this website.
+                When I'm not behind my computer, I play and have played competitive badminton for a long time, Also walking is my main way of exercising since I can listen to audio books while I do so. I'm a CMAS certified Diver I also watch tv shows and documentaries when i have time.
+                </p>
+
             </div>
         
         
